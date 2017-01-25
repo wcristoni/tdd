2b72686e9297d8a6a9893f67672a3358bd5d7245
Wilson C Neto <Wilson@MacBook-Pro-de-Wilson.local>
2017-01-25 16:51:31 -0200

    Adicionando JaCoco Plugin e Teste Unitario

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -6,4 +6,10 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
+	@Test
+	public void testDebitWithSufficientFunds(){
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5);
+		Assert.assertEquals(5.0,amount);
+	}
 }
