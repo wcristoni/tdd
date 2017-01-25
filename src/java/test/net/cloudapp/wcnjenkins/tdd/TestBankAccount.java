95949b9b3763f45e9a2a2e57f5e56168ef15cb9c
Wilson C Neto <Wilson@MacBook-Pro-de-Wilson.local>
2017-01-25 18:43:13 -0200

    Ajuste dos valores de referencia

--- src/java/test/net/cloudapp/wcnjenkins/tdd/TestBankAccount.java
+++ src/java/test/net/cloudapp/wcnjenkins/tdd/TestBankAccount.java
@@ -17,13 +17,13 @@ public class TestBankAccount {
 	public void testDebitWithInsufficientFunds(){
 		BankAccount account = new BankAccount(10);
 		double amount = account.debit(11);
-		Assert.assertEquals(10.0,amount);
+		Assert.assertEquals(-1.0,amount);
 	}
 
 	@Test
 	public void testDebitWithNegativeFunds(){
 		BankAccount account = new BankAccount(-10);
 		double amount = account.debit(5);
-		Assert.assertEquals(-10.0,amount);
+		Assert.assertEquals(-5.0,amount);
 	}
 }
