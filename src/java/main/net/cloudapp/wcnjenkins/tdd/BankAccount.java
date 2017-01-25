f20a75fa4e860c67ca512019f229e36ebf21eddb
Wilson C Neto <Wilson@MacBook-Pro-de-Wilson.local>
2017-01-25 18:01:03 -0200

    Organizando o projeto para deixar no padrão maven com Junit

--- src/java/main/net/cloudapp/wcnjenkins/tdd/BankAccount.java
+++ src/java/main/net/cloudapp/wcnjenkins/tdd/BankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package net.cloudapp.wcnjenkins;
 
 public class BankAccount {
 
--- src/java/test/net/cloudapp/wcnjenkins/tdd/TestBankAccount.java
+++ src/java/test/net/cloudapp/wcnjenkins/tdd/TestBankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package net.cloudapp.wcnjenkins;
 
 import junit.framework.Assert;
 import org.junit.Before;
